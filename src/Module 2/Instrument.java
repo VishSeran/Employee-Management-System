35fbe12bea9aa57b9a747953d4b714c9db51146e
VishSeran <vishwa.sovis@gmail.com>
2026-02-11 13:13:12 +0530

    Instrument.java updated

---
+++ src/Module 2/Instrument.java
@@ -0,0 +1,93 @@
+// Step 1: Create an abstract class Instrument
+abstract class Instrument {
+
+    private String name;
+    protected int year;
+
+    public Instrument(String name, int year){
+
+        this.name = name;
+        this.year = year;
+    }
+
+    public abstract String play();
+
+    public String getInstrumentDetails(){
+        return ("name" + this.name + "\nyear: " + this.year);
+    }
+
+}
+
+// Step 2: Create an interface Tunable
+interface Tunable{
+    String tune();
+    String adjustPitch(boolean up);
+}
+
+interface Maintainable{
+    String clean();
+    String inspect();
+}
+
+class StringedInstrument extends Instrument {
+
+    private int numberOfStrings;
+    
+    public StringedInstrument(String name, int year, int numberOfStrings) {
+        super(name, year);
+        this.numberOfStrings = numberOfStrings;
+    }
+
+    @Override
+    public String play(){
+        return "Playing " + numberOfStrings + " strings instrument";
+    }
+
+    @Override
+    public String getInstrumentDetails(){
+        return super.getInstrumentDetails() + "No of strings: " + numberOfStrings;
+    }
+
+    public int getNumberOfStrings (){
+        return numberOfStrings;
+    }
+}
+
+class Guitar extends StringedInstrument implements Tunable, Maintainable {
+
+    private String guitarType; //classical, electric, accoustic
+
+    public Guitar (String name, int year, int numberOFStrings, String guitarType){
+        super(name, year, numberOFStrings);
+        this.guitarType = guitarType;
+    }
+
+    public String play(){
+        return ("guitar playing");
+    }
+
+    public String tune(){
+        return "tuning start";
+    }
+
+    public String adjustPitch(boolean up){
+        if(up){
+            return "pitched upwared adjusted";
+        } else {
+            return "already adjusted";
+        } 
+
+        
+    }
+
+    public String clean(){
+        return "Guitar is cleaned";
+    }
+
+    public String inspect(){
+        return "Inspect is started now";
+    }
+
+
+
+}
