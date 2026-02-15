f62ed019fbca5f6f017450c7fcea56ea6714f66b
VishSeran <vishwa.sovis@gmail.com>
2026-02-15 12:45:07 +0530

    books sorted function updated

--- src/Module3/Library_Management_ArrayList/LibraryManagement.java
+++ src/Module3/Library_Management_ArrayList/LibraryManagement.java
@@ -1,6 +1,7 @@
 
-import java.awt.font.NumericShaper;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class LibraryManagement {
@@ -53,7 +54,7 @@ public class LibraryManagement {
 
                     case 5:
                         System.out.println("Retrning processing...");
-                        returnBook(scanner, library);
+                        returnBackBook(scanner, library);
 
                 }
 
@@ -168,9 +169,6 @@ public class LibraryManagement {
 
     private static void returnBackBook(Scanner scanner, ArrayList<Book> library) {
         System.out.println("Enter the book name");
-        String bookName = scanner.nextLine();
-
-        boolean isReturned = false;
 
         if (library.isEmpty()) {
             System.out.println("Library is empty now. thank you for ur support!");
@@ -205,15 +203,79 @@ public class LibraryManagement {
 
             Book selectedBook = library.get(number - 1);
 
-            if(selectedBook.returnBook()){
+            if (selectedBook.returnBook()) {
                 System.out.println(selectedBook + " Book return Suucessfull!");
-            }else{
+            } else {
                 System.out.println("Book return failed!");
             }
 
-
         } catch (NumberFormatException e) {
             System.out.println("Invalid input type!");
+        }
+
+    }
+
+    private static void sortBook(Scanner scanner, ArrayList<Book> library) {
+
+        System.out.println("Sorting processing...");
+
+        if (library.isEmpty()) {
+            System.out.println("Library is empty for now");
+            return;
+        }
+
+        System.out.println("Select the sort order: ");
+        System.out.println("1. Title");
+        System.out.println("2. Author");
+        System.out.println("3. Publication Year");
+        System.out.print("Enter your choice: ");
+
+        try {
+
+            int choice = Integer.parseInt(scanner.nextLine());
+
+            switch (choice) {
+                case 1:
+                    Collections.sort(library, new Comparator<Book>(){
+                        @Override
+                        public int compare(Book b1, Book b2){
+                            return b1.getTitle().compareToIgnoreCase(b2.getTitle());
+                        }
+                    });
+                    System.out.println("Sorted by title: ");
+                    break;
+
+                case 2: 
+                    Collections.sort(library, new Comparator<Book> () {
+                        @Override
+                        public int compare(Book b1, Book b2){
+                            return b1.getAuthor().compareToIgnoreCase(b2.getAuthor());
+                        }
+                    });
+
+                    System.out.println("Sorted by author: ");
+                    break;
+
+                case 3:
+                    Collections.sort(library, new Comparator<Book>(){
+                        @Override
+                        public int compare(Book b1, Book b2){
+                            return Integer.compare(b1.getPublicationYear(), b2.getPublicationYear());
+                        }
+                    });
+                    System.out.println("Sorted by publication year: ");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+                    return;
+            }
+
+            getBooks(library);
+
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid type input!");
         }
 
     }
